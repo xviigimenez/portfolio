90fc9aa516a19caee8bbbc00c881d041ba02bef4
xviigimenez <gimenez@tuta.io>
2026-05-02 14:57:52 -0300

    Configuração inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div>
+      <h1 className="text-center text-3xl font-bold">Site sob construção!</h1>
+      <img
+        src="/src/assets/under-construction.gif"
+        alt="GIF de um desenho animado"
+      />
+      </div>
+    </div>
+  );
+}
+
+export default App
