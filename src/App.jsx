3cc265671cb7ac912487b4afde9c0494b453e8c5
xviigimenez <gimenez@tuta.io>
2026-05-02 21:44:35 -0300

    Cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-function App() {
+import Header from './components/Header';
+
+export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div>
-        <h2 className="text-center text-2xl font-bold">Site sob construção!</h2>
-          <img
-            src="/under-construction.gif"
-            alt="GIF de um desenho animado"
-          />
+    <>
+      <Header />
+      <div className="h-screen flex items-center justify-center">
+        <div>
+          <h2 className="text-center text-2xl font-bold">
+            Site sob construção!
+          </h2>
+          <img src="/under-construction.gif" alt="GIF de um desenho animado" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between px-6 h-14 border-b border-gray-200">
+      <div>
+        <h1 className="text-2xl">
+          gustavo<span className="text-lg">.xyz.br</span>
+        </h1>
+      </div>
+      <nav className="hidden md:flex items-center gap-2">
+        <a href="#sobre" className="text-lg text-gray-500 hover:text-black">
+          sobre
+        </a>
+        <a href="#projetos" className="text-lg text-gray-500 hover:text-black">
+          projetos
+        </a>
+        <a href="#eventos" className="text-lg text-gray-500 hover:text-black">
+          eventos
+        </a>
+        <a href="#contatos" className="text-lg text-gray-500 hover:text-black">
+          contatos
+        </a>
+      </nav>
+    </header>
+  );
+}
