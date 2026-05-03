27668883e4bf83c0f7276198d33d055540f446fd
xviigimenez <gimenez@tuta.io>
2026-05-02 21:43:42 -0300

    Configuração do Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       globals: globals.browser,
