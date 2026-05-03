15910dca225cf725e73a9c890a20a20bab62c005
xviigimenez <gimenez@tuta.io>
2026-05-03 18:46:29 -0300

    Header fixo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-6 h-14 border-b border-gray-200">
+    <header className="sticky top-0 z-50 flex items-center justify-between px-6 h-14 border-b border-gray-200">
       <div>
         <h1 className="text-2xl">
           gustavo<span className="text-lg">.xyz.br</span>
         </h1>
       </div>
-      <nav className="hidden md:flex items-center gap-2">
+      <nav className="hidden md:flex items-center gap-4">
         <a href="#sobre" className="text-lg text-gray-500 hover:text-black">
           sobre
         </a>
