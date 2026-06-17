fb2297ac50935df70e1787b64370d35f43c8629d
xviigimenez <gimenez@tuta.io>
2026-06-17 17:29:28 -0300

    chore: Removidos os resquícios da versão inicial do site

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,6 @@ import Contato from './components/Contato';
 import Footer from './components/Footer';
 import Curriculo from './components/Curriculo';
 
-export function UnderConstruction() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <div>
-        <h2 className="text-center text-2xl font-bold">Site sob construção!</h2>
-        <img src="/under-construction.gif" alt="GIF de um desenho animado" />
-      </div>
-    </div>
-  );
-}
-
 export default function App() {
   const [modo, setModo] = useState('landing');
 
